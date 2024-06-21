ee68ab425d2205eb127765762f3beea0dda70753
Pramuditha <pramudithagn@gmail.com>
2024-06-21 19:54:42 +0530

    first commit

---
+++ src/main/java/com/pramu/fasteats/FastEatsApplication.java
@@ -0,0 +1,13 @@
+package com.pramu.fasteats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastEatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastEatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pramu/fasteats/FastEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramu.fasteats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastEatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
