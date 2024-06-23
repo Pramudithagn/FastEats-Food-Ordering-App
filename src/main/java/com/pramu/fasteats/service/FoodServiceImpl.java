52230c64f245346d566ac8a1b82b050cf7322cda
Pramuditha <pramudithagn@gmail.com>
2024-06-23 12:52:39 +0530

    food service & repo

--- src/main/java/com/pramu/fasteats/service/FoodServiceImpl.java
+++ src/main/java/com/pramu/fasteats/service/FoodServiceImpl.java
@@ -5,10 +5,13 @@ import com.pramu.fasteats.model.Food;
 import com.pramu.fasteats.model.Restaurant;
 import com.pramu.fasteats.repository.FoodRepository;
 import com.pramu.fasteats.request.FoodRequest;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class FoodServiceImpl implements FoodService {
@@ -18,7 +21,9 @@ public class FoodServiceImpl implements FoodService {
 
     @Override
     public Food findFoodById(Long id) {
-        return null;
+        Optional<Food> food = foodRepository.findById(id);
+        if (food.isEmpty()) throw new EntityNotFoundException("Food not found !");
+        return food.get();
     }
 
     @Override
@@ -28,12 +33,48 @@ public class FoodServiceImpl implements FoodService {
 
     @Override
     public List<Food> searchFood(String keyword) {
-        return List.of();
+        return foodRepository.searchFood(keyword);
     }
 
     @Override
     public List<Food> getRestaurantFoods(Long restaurantId, String category, boolean isVeg, boolean isNonVeg, boolean isSeasonal) throws Exception {
-        return List.of();
+        List<Food> foods = foodRepository.findByRestaurantId(restaurantId);
+
+        if(isVeg){
+            foods = filterByVegetarian(foods, isVeg);
+        }
+        if(isNonVeg){
+            foods = filterByNonVegetarian(foods, isNonVeg);
+        }
+        if(isSeasonal){
+            foods = filterBySeasonal(foods, isSeasonal);
+        }
+        if (category != null && !category.equals("")) {
+            foods = filterByCategory(foods, category);
+        }
+
+        return foods;
+    }
+
+    private List<Food> filterByCategory(List<Food> foods, String category) {
+        return foods.stream().filter(food -> {
+            if(food.getCategory() != null){
+                return food.getCategory().getName().equals(category);
+            }
+            return false;
+        }).collect(Collectors.toList());
+    }
+
+    private List<Food> filterBySeasonal(List<Food> foods, boolean isSeasonal) {
+        return foods.stream().filter(food -> food.isSeasonal()==isSeasonal).collect(Collectors.toList());
+    }
+
+    private List<Food> filterByNonVegetarian(List<Food> foods, boolean isNonVeg) {
+        return foods.stream().filter(food -> food.isVegetarian()==false).collect(Collectors.toList());
+    }
+
+    private List<Food> filterByVegetarian(List<Food> foods, boolean isVeg) {
+        return foods.stream().filter(food -> food.isVegetarian()==isVeg).collect(Collectors.toList());
     }
 
     @Override
@@ -57,11 +98,16 @@ public class FoodServiceImpl implements FoodService {
 
     @Override
     public Food updateAvailabilityStatus(Long foodId) throws Exception {
-        return null;
+        Food food = findFoodById(foodId);
+        food.setAvailable(!food.isAvailable());
+
+        return foodRepository.save(food);
     }
 
     @Override
     public void deleteFood(Long foodId) throws Exception {
-
+        Food food = findFoodById(foodId);
+        food.setRestaurant(null);
+        foodRepository.save(food);
     }
 }
