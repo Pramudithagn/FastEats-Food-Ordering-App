2feae53006c06f742f4207707cf9f4039564e7d7
Pramuditha Gunaratne <pramudithagn@gmail.com>
2024-07-24 08:47:29 -0700

    event model & repo

---
+++ src/main/java/com/pramu/fasteats/model/Event.java
@@ -0,0 +1,31 @@
+package com.pramu.fasteats.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String image;
+
+    private String startedAt;
+
+    private String endsAt;
+
+    private String name;
+
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private String location;
+
+}
---
+++ src/main/java/com/pramu/fasteats/repository/EventRepository.java
@@ -0,0 +1,12 @@
+package com.pramu.fasteats.repository;
+
+import com.pramu.fasteats.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    public List<Event> findEventsByRestaurantId(Long id);
+
+}
